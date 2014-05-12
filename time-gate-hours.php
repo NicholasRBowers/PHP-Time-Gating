9154b039e7fa84321d55519e06580b27b0210284
NicholasRBowers <nicholas.ryan.bowers@gmail.com>
2014-05-12 13:51:02 -0400

    Stable version.

--- time-gate-hours.php
+++ time-gate-hours.php
@@ -5,17 +5,17 @@ date_default_timezone_set('America/New_York');
 
 // Define daily hours for which the time gate will be open.
 // Times must be in 24-hour format, separated by dash.
-// If the gate is to be open for the whole day, leave the array for that day empty.
-// If the gate is to be closed for the whole day, set to 00:00-00:00.
+// If the gate is to be closed for the whole day, leave the array for that day empty.
+// If the gate is to be open for the whole day, set to 00:00-00:00.
 // If there are multiple open times in one day, enter time ranges separated by a comma.
 // If the gate is open late (ie. 6pm - 1am), add hours after midnight to the next day (ie. 00:00-1:00).
 $openHours = array(
   'mon' => array('11:00-20:30'),                  // Normal.
   'tue' => array('11:00-16:00', '18:00-22:30'),   // Multiple open times.
-  'wed' => array(''),                               // Open all day.
-  'thu' => array('00:00-2:00','11:00-23:59'),                  // Open past midnight.
+  'wed' => array('00:00-00:00'),                  // Open all day.
+  'thu' => array('00:00-2:00','11:00-23:59'),     // Open past midnight.
   'fri' => array('00:00-2:00', '11:00-20:30'),
-  'sat' => array('11:00-20:00'),
+  'sat' => array(''),                             // Closed all day.
   'sun' => array('11:00-20:30')
 );
 
--- time-gate.php
+++ time-gate.php
@@ -1,30 +1,16 @@
 <?php
 require_once('time-gate-hours.php');
 
-print_r(isOpenNow($openHours, true));
-
-function isOpen($time, $date, $openHours, $getDetails = false, $exceptions) { // Check if the gate is open during a specific time.
+function isOpen($time, $date, $openHours, $exceptions) { // Check if the gate is open during a specific time.
   $time = strtotime($time); 																									// Prepare variables.
   $days = array('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun');
   $day = strtolower(date("D", strtotime($date)));
 
   if (isClosedAllDay($date, $openHours, $exceptions)) { 											// Check if the gate is closed all day.
-  	if ($getDetails) {
-
-    }
-    // If getDetails
-      // go to the first entry of the next day
-      // go to the last entry of the previous day
-      // Keep going until you find SOMETHING.
     return false;
   }
 
-  if (count($openHours[$day]) === 0) { 																				// Check if the gate is open all day.
-    // if getDetails,
-      // go to next day.
-      // if the next day's first open entry is 00:00
-        // return it's corresponding ending limit
-      // else, return 00:00.
+  if ($openHours[$day][0] == '00:00-00:00') { 															  // Check if the gate is open all day.
   	return true;
   } else {
   	foreach($openHours[$day] as $range) { 																		// Check if the gate is open.
@@ -32,31 +18,10 @@ function isOpen($time, $date, $openHours, $getDetails = false, $exceptions) { //
   		$start = strtotime($range[0]);
   		$end = strtotime($range[1]);
   		if (($start <= $time) && ($time <= $end)) {                             // If the gate is open,
-  			if ($getDetails) {
-          if ($end === strtotime('23:59') || $end === strtotime('23:59:59')) { // The case when the gate is open past midnight.
-            $tomorrow = array_search($day, $days) + 1;
-            if ($tomorrow === 7) {
-              $tomorrow = 0;
-            }
-            if (isOpen('00:00', date('m/d' , strtotime($date."+1 days")), $openHours, false, $exceptions)) {
-              $range = explode("-", $openHours[$days[$tomorrow]][0]);
-              // Need to make sure that $end doesn't equal 00:00 (RE: adding a day)
-              $end = strtotime($range[1]."+1 days");
-            }
-          }
-          // Change these back to just timestamps.
-  				return [true, date('l F j, Y g:i a', $start), date('l F j, Y g:i a', $end)];
-  			} else {
-  				return true;
+  			return true;
   			}
   		}
   	}
-  	if ($getDetails) {
-      // Where are you in the day's array?  These are open values, not closed values!
-        // Beginning - go to the previous day's ending value and the next slot's starting value.
-        // End - go to the previous slot's ending value and the next day's starting value.
-        // Middle - go to the previous slot's ending value and the next slot's starting value.
-        // Beginning and End - 
   		return [false, '', ''];
   	} else {
   		return false;
@@ -77,17 +42,17 @@ function isClosedAllDay($date, $openHours, $exceptions) { // Check if gate is cl
 		}
 	}
 
-	if ($openHours[$day][0] == '00:00-00:00') { 						// Check if the store is closed all day.
+	if (count($openHours[$day]) === 0) { 						        // Check if the store is closed all day.
 		return true;
 	}
 	return false;
 }
 
-function isOpenNow($openHours, $getDetails = false, $exceptions) { // Check if the gate is open now.
+function isOpenNow($openHours, $exceptions) { // Check if the gate is open now.
 	$time = date("G:i");
 	$date = date("m/d/Y");
 
-	return isOpen($time, $date, $openHours, $getDetails, $exceptions);
+	return isOpen($time, $date, $openHours, $exceptions);
 }
 
 ?>
